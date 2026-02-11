916102fe20959142e8959078ec027ca5651a2ae3
Shuvo Halder <shuvohalder499@gmail.com>
2026-02-11 23:00:19 +0600

    Create a new project "Expense Tracker"

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import {sql} from './config/db.js';
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+async function initDB() {
+    try {
+        await sql`CREATE TABLE IF NOT EXISTS transactions (
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at Date NOT NULL DEFAULT CURRENT_DATE
+        )`;
+
+        console.log('Database initialized successfully');
+    } catch (error) {
+        console.error('Error initializing database:', error);
+        process.exit(1); // Status code 1 means failure and 0 means success.
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+initDB().then(() => {
+    app.listen(PORT,() =>{
+    console.log(`Server is running on port: ${PORT}`);
+});
+})
