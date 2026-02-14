19ac0bb87612e050f81632258607defa42e9d18e
Shuvo Halder <shuvohalder499@gmail.com>
2026-02-14 21:47:16 +0600

    The rate limit is working successfully in this expense project

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import {sql} from './config/db.js';
+import rateLimiter from './middleware/rateLimiter.js';
 dotenv.config();
 
 const app = express();
 
 // middleware
+app.use(rateLimiter)
 app.use(express.json())
 
 const PORT = process.env.PORT || 5001;
