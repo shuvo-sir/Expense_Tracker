0ab2a717f237cd52b7231be62484c47929284d1f
Shuvo Halder <shuvohalder499@gmail.com>
2026-02-15 22:33:54 +0600

    create the some basics

---
+++ mobile/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        backgroundColor: "red",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
